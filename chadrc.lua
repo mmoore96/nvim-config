50a2b08d84b49dc571b37b0f8609788be3bd383e
Michael Moore <GMMoore96@outlook.com>
2024-02-23 15:35:52 -0500

    refactor(chadrc): ♻️ streamline NVChad config and switch theme to onedark
    Significantly simplified the NVChad configuration within `chadrc.lua` by removing the detailed startup dashboard graphics and transparency settings. This change aims to enhance startup speed and reduce visual clutter, focusing on a cleaner and more straightforward user interface.
    
    - Switched the UI theme from 'catppuccin' to 'onedark' to align with a more universally preferred aesthetic that offers better readability and a more consistent developer experience across various environments.
    - Removed the custom ASCII art header and the transparency configuration from the UI settings to streamline the configuration file, making it easier to maintain and understand for new and existing users.
    - Retained essential configurations for plugins and mappings by pointing to `custom.plugins` and including mappings from `custom.mappings`, ensuring that key customizations and extensions remain functional and accessible.

--- chadrc.lua
+++ chadrc.lua
@@ -1,27 +1,7 @@
----@type ChadrcConfig 
- local M = {}
- M.ui = {
-  theme = 'catppuccin',
-  transparency = true,
- nvdash = {
-  load_on_startup = false,
-  header = {
-    "                                                                        ",
-    "                                                          OOO           ",
-    "                                                         OO OO    OOO   ",
-    " PPP                                                      000    00 00  ",
-    " P PP             T                T   I                          OOO   ",
-    " P PP RRR   OO  TTTTT  EE    CCC TTTTT   VV  VV  EE      ** **          ",
-    " PPP  R  R O  O   T   E  E  C      T   I V    V E  E     ** **   ** **  ",
-    " P    R    O  O   T   EEEE C       T   I  V  V  EEEE    **   ** **  **  ",
-    " P    R    O  O   T   E     C      T   I   VV   E      **     ***    ** ",
-    " P    R     OO    T   EEEE   CCC   T   I   VV   EEEE  ***    ****  ***  ",
-    "                                                            ** ** ***   ",
-    "                                                            ** **       ",
-    "                                                                        ",
-  },
-},
-}
- M.plugins = "custom.plugins"
- M.mappings = require "custom.mappings"
- return M
+---@type ChadrcConfig
+local M = {}
+
+M.ui = { theme = 'onedark' }
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
+return M
