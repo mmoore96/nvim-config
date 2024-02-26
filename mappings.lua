4d1f3cc306ea84ac05226dd738273cb2115065ba
Michael Moore <GMMoore96@outlook.com>
2024-02-26 17:24:36 -0500

    feat(mappings): ✨ integrate harpoon key mappings for enhanced navigation
    - Implemented key mappings for the 'harpoon' plugin within `mappings.lua`, enriching the Neovim user experience with efficient file navigation and quick access functionalities.
    - Added key bindings for:
      - `<leader>A` to append the current file to the 'harpoon' list, facilitating a dynamic workflow for tracking and returning to important files.
      - `<leader>a` to toggle the 'harpoon' quick menu, offering a streamlined interface for navigating among bookmarked files.
      - `<leader>1` through `<leader>5` for immediate navigation to the first five bookmarked files, significantly enhancing the ease of moving between frequently accessed documents.
    - These mappings not only improve productivity by reducing the time spent on file navigation but also integrate seamlessly with the existing development environment, allowing developers to maintain focus on their code.
    - The introduction of these 'harpoon' key mappings demonstrates a commitment to leveraging the latest tools and plugins to enhance coding efficiency and project management within the Neovim ecosystem.
    
    Branch: feature/integrate-harpoon-plugin
    
    Pull Request: !2

--- mappings.lua
+++ mappings.lua
@@ -7,6 +7,18 @@ M.lazygit = {
   },
 }
 
+M.harpoon = {
+  n = {
+    ["<leader>A"] = { function() require("harpoon"):list():append() end, "Harpoon: Add file" },
+    ["<leader>a"] = { function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, "Harpoon: Toggle quick menu" },
+    ["<leader>1"] = { function() require("harpoon"):list():select(1) end, "Harpoon: Go to file 1" },
+    ["<leader>2"] = { function() require("harpoon"):list():select(2) end, "Harpoon: Go to file 2" },
+    ["<leader>3"] = { function() require("harpoon"):list():select(3) end, "Harpoon: Go to file 3" },
+    ["<leader>4"] = { function() require("harpoon"):list():select(4) end, "Harpoon: Go to file 4" },
+    ["<leader>5"] = { function() require("harpoon"):list():select(5) end, "Harpoon: Go to file 5" },
+  },
+}
+
 M.dap = {
   plugin = true,
   n = {
