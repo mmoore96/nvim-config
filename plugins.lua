96bdbfcc2d53be686636475996d7ce8fb2f81b3f
Michael Moore <GMMoore96@outlook.com>
2024-03-01 17:43:06 -0500

    feat(plugins): ✨ integrate GitHub Copilot
    - Added `github/copilot.vim` plugin to `plugins.lua`, integrating GitHub Copilot directly into the Neovim development environment. This powerful tool offers AI-powered code completion and suggestions, enhancing productivity and code quality.
    - Configured the plugin to disable the default Tab mapping used by Copilot for accepting suggestions. This adjustment prevents conflicts with existing Tab behaviors in the Neovim setup, ensuring a seamless integration with other plugins and native functionalities.
    - Established custom mappings for Copilot commands to adapt to the personalized development workflow. These mappings are designed to offer intuitive access to Copilot's features without disrupting the established coding practices.
    - By setting `copilot_no_tab_map`, `copilot_assume_mapped`, and `copilot_tab_fallback` configurations, we maintain control over the interaction model with Copilot, tailoring it to fit my development style and preferences.
    - This integration marks a significant enhancement to my coding environment, leveraging the latest advancements in AI to support and improve the software development process.
    
    Branch: feature/integrate-copilot
    
    Pull Request: !4

--- plugins.lua
+++ plugins.lua
@@ -33,6 +33,17 @@ local plugins = {
     end
   },
   {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()  -- Mapping tab is already used by NvChad
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+      -- The mapping is set to other key, see custom/lua/mappings
+      -- or run <leader>ch to see copilot mapping section
+    end
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
